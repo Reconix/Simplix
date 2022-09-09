67f6cbf75d7a8678d3be135266d9119a01a46931
Reconix <Reconix@users.noreply.github.com>
2022-09-09 19:09:17 +0100

    and so it begins..
    Removed old stuff

---
+++ _blank.php
@@ -0,0 +1,84 @@
+<!DOCTYPE html>
+<html lang="en">
+
+<head>
+  <!--=:: Meta Stuff ::=-->
+  <meta charset="utf-8">
+  <meta content="width=device-width, initial-scale=1.0" name="viewport">
+  <meta name="description" content="A basic, simple, clean theme.">
+  <meta name="keywords" content="simple, clean, basic">
+  <meta name="author" content="Reconix">
+
+  <!--=:: Title ::=-->
+  <title>Simplix</title>
+
+  <!--=:: Favicons ::=-->
+
+  <!--=:: Fonts ::=-->
+
+  <!--=:: CSS Dependencies ::=-->
+  <link href="assets/css/bootstrap/bootstrap.min.css" rel="stylesheet">
+
+  <!--=:: CSS Theme Syles ::=-->
+  <link href="assets/css/style.css" rel="stylesheet">
+
+</head>
+
+<body>
+
+<?php
+ /*
+
+ We might do somthing with you later...
+
+  <!--=:: Top Bar ::=-->
+  <section id="topbar" class="d-flex align-items-center">
+    <div class="container d-flex justify-content-center justify-content-md-between">
+      <div class="contact-info d-flex align-items-center">
+        <i class="bi bi-envelope-fill"></i><a href="mailto:contact@simplex.net">info@simplex.net</a>
+        <i class="bi bi-phone-fill phone-icon"></i> 12345678912
+      </div>
+      <div class="social-links d-none d-md-block">
+        <a href="#" class="facebook"><i class="bi bi-facebook"></i></a>
+        <a href="#" class="twitter"><i class="bi bi-twitter"></i></a>
+        <a href="#" class="instagram"><i class="bi bi-instagram"></i></a>
+        <a href="#" class="linkedin"><i class="bi bi-linkedin"></i></i></a>
+      </div>
+    </div>
+  </section>
+*/
+?>
+
+  <?php include_once('inc/header.php');?>
+
+  <!--=:: Slider ::=-->
+
+  <!--=:: Content Section ::=-->
+
+  <main id="main-content">
+
+   <!--=:: Sections ::=-->
+
+    <!--- Simple --->
+    <section id="simples" class="simples">
+      <div class="container">
+        <div style="margin:50px 5px 300px 10px;">
+        <h1>Hello World</h1>
+        <p>
+          I'm not great with themes so take this with a pintch of salt, the
+          theme was originally for ForgeIgniter.
+        </p>
+        </div>
+      </div>
+    </section>
+
+  </main>
+  <?php include_once('inc/footer.php');?>
+
+  <!--=:: JS Dependencies ::=--->
+  <script src="assets/js/bootstrap/bootstrap.bundle.min.js"></script>
+
+  <!--=:: JS Theme ::=--->
+
+</body>
+</html>
